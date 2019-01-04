cd0b626c22252776afc388e4a7e9f0d81f376ced
Hoang Van Loc <lochoang75@gmail.com>
2019-01-04 22:52:49 +0700

    add prolog

---
+++ box.pl
@@ -0,0 +1,19 @@
+% goal 
+%                   D 
+%                   -
+%                   C
+%                   -
+%                   B
+%                   -
+%                   A
+%                   -
+on(b, a).
+on(c, table).
+on(d, table).
+on(a, table).
+clear(c).
+clear(d).
+clear(b).
+clear(B) :- not(on(_X,B)
+Unstack(a,b) :- 
+
